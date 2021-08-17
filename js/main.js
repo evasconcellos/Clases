c082b0c17a8ff410668a7b9a5cc6748f9ccba501
Erik Vasconcellos <erikvasconcellos@gmail.com>
2021-08-17 19:44:03 -0300

    Entrega nro 3

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let nombre = prompt("Ingresá tu nombre");
+if (nombre !== "") {
+  alert("Hola " + nombre + ", bienvenido.");
+
+  let edad = prompt("Ingresá tu edad");
+  if (edad >= 18) {
+    alert("Tenés " + edad + ", excelente.");
+    let elegirNumer = parseInt(prompt("Ahora elige un número del 1 al 5"));
+
+    switch (elegirNumer) {
+      case 1:
+        alert("Elegiste el 1");
+        break;
+      case 2:
+        alert("Elegiste el 2");
+        break;
+      case 3:
+        alert("Elegiste el 3");
+        break;
+      case 4:
+        alert("Elegiste el 4");
+        break;
+      case 5:
+        alert("Elegiste el 5");
+        break;
+
+      default:
+        alert("Error!");
+    }
+
+    if (elegirNumer <= 5) {
+      let sumaNumer = prompt("Al número elegido, sumale otro");
+      alert("Tu resultado es " + (parseInt(elegirNumer) + parseInt(sumaNumer)));
+    } else {
+      alert(
+        "El número ingresado no corresponde a lo pedido, vuelve a intentarlo."
+      );
+    }
+  } else {
+    alert("Solo puedes continuar si eres mayor de 18 años");
+  }
+} else {
+  alert("Debes escribir tu nombre para continuar");
+}
